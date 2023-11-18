790d2767fa81e649ce40bff358c6982e89736fda
MirantyAnjaniPutri <miranty.anjani@ui.ac.id>
2023-11-18 10:14:34 +0700

    Init karena yang sebelumnya error

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      {/* Image */}
+      <div>
+        <div>
+          <h2>Heading</h2>
+          <p>Message</p>
+          <button>Book</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
